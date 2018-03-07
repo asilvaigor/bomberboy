16d80237d0c4dce10c6deffb5be8a40e50d44e4d
Heladio Lopes <heladio.lopes@gmail.com>
2018-03-07 19:29:38 -0300

    Adding the initial game Engine

--- source/core/engine/Engine.py
+++ source/core/engine/Engine.py
@@ -1,3 +1,8 @@
+import pygame
+import sys
+from pygame.locals import *
+
+
 class Engine:
     """
     Game engine class. This is the class that runs the game and contains the
@@ -5,7 +10,30 @@ class Engine:
     """
 
     def __init__(self):
-        pass
+        """
+        Set variables required for window creation.
+        """
+        self.screen_width = 500
+        self.screen_height = 700
+        self.game_name = "Bomber Boy"
+        self.window_screen = None
 
     def play(self):
-        pass
+        self.start_window()
+
+        self.main_loop()
+
+    def start_window(self):
+        """
+        Creates the game play window
+        """
+        pygame.init()
+        self.window_screen = pygame.display.set_mode((self.screen_height, self.screen_width), 0, 32)
+        pygame.display.set_caption(self.game_name)
+
+    def main_loop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
