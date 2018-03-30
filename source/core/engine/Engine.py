65f6e69ac7bab1d0bdfb8eb012c25647f6ed5657
Heladio Lopes <heladio.lopes@gmail.com>
2018-03-30 10:12:11 -0300

    Creating games dynamically

--- source/core/engine/Engine.py
+++ source/core/engine/Engine.py
@@ -4,7 +4,9 @@ from pygame.locals import *
 from source.core.utils.Constants import *
 from source.core.ui.Menu import Menu
 from source.core.engine.Match import Match
-#from source.core.ui.Pause import Pause
+
+
+# from source.core.ui.Pause import Pause
 
 
 class Engine:
@@ -28,17 +30,25 @@ class Engine:
         self.__song = pygame.mixer.Sound("assets/song/song.ogg")
 
         self.__state = MENU
-        self.__menu = Menu()
-        self.__match = Match()
+        self.__menu = None
+        self.__match = None
 
     def play(self):
         # self.__song.play(-1)
         while True:
             if self.__state == MENU:
+                if not self.__menu:  # Verify that the pointer is null
+                    self.__menu = Menu()
+                    del self.__match
+                    self.__match = None
                 self.__menu.draw(self.__surface)
                 self.__state = self.__menu.update()
 
             elif self.__state == PLAYING_SINGLE:
+                if not self.__match:  # Verify that the pointer is null
+                    self.__match = Match()
+                    del self.__menu
+                    self.__menu = None
                 self.__state = self.__match.play(self.fpsClock, self.__surface)
 
             elif self.__state == FINISH:
